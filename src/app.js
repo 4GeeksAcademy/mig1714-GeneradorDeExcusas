0710e3d9c91423a0387cf15f8ea016e2bf48ee2f
mig1714 <63379958+mig1714@users.noreply.github.com>
2026-05-26 04:18:44 +0000

    Creada funcion generadora de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const selector = (array)=>{
+
+
+  let elegir = Math.floor(Math.random()*array.length );
+
+  return elegir
+
+
+}
+
+
+const excusa =()=>{
+
+  return `${who[selector(who)]} ${action[selector(action)]} ${what[selector(what)]} ${when[selector(when)]}`;
+}
+
+console.log(selector(who));
+
+console.log(excusa());
